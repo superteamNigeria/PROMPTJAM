24c9e638632736e92af4ec7d50ea39ecf45b60b8
Leeyah-123 <junaidaaliyah260@gmail.com>
2025-01-27 05:06:39 +0100

    Add day_2 folder

---
+++ Promptjam_Aaliyah_Junaid/day_2/src/components/WalletConnectionProvider.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { FC, ReactNode, useMemo } from 'react';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+interface WalletConnectionProviderProps {
+  children: ReactNode;
+}
+
+export const WalletConnectionProvider: FC<WalletConnectionProviderProps> = ({
+  children,
+}) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
